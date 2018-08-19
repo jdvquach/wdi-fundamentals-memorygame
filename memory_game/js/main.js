8787ba6c41e27ea006d4e6e9bb29c3383c79ba8b
John Quach <John.DV.Quach@gmail.com>
2018-08-19 15:59:27 +1000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,24 @@
-var cardOne = "queen";
+/*var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "kng";
 
 console.log("User flipped " + cardOne);
+*/
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne;
+var cardTwo;
+var checkWinner;
+
+cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+// console.log(cardsInPlay.length);
+  checkWinner = cardsInPlay[0] === cardsInPlay[1] ? alert("You found a match!") : alert("Sorry, try again.")
+}
